77c49ae33826421cdb56fb909c53bb35cabff5a7
Veena-123-dotcom <lpriyanka489@gmail.com>
2024-11-28 11:51:49 +0530

    commitfiles

---
+++ src/main/java/githubRepositiory/demo1.java
@@ -0,0 +1,10 @@
+package githubRepositiory;
+
+public class demo1
+{
+	public static void main(String[]args)
+	{
+		System.out.println("rempte the repository");
+	}
+
+}
---
+++ src/main/java/githubRepositiory/samplegitDmo.java
@@ -0,0 +1,10 @@
+package githubRepositiory;
+
+public class samplegitDmo 
+{
+	  public static void main(String[]args)
+	  {
+		       System.out.println("demo1 programe file");
+	  }
+
+}
