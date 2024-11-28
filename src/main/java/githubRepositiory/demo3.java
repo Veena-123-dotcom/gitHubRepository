1de79b5118d1fff1a043535066c95e9f37a113cb
Veena-123-dotcom <lpriyanka489@gmail.com>
2024-11-28 12:23:59 +0530

    msg

---
+++ src/main/java/githubRepositiory/demo3.java
@@ -0,0 +1,10 @@
+package githubRepositiory;
+
+public class demo3
+{
+	    public  static void main(String []args)
+	    {
+	    	  System.out.println("demo3");
+	    }
+
+}
---
+++ src/main/java/githubRepositiory/demo4.java
@@ -0,0 +1,9 @@
+package githubRepositiory;
+
+public class demo4 
+{
+	public static void main(String []args)
+	{
+	System.out.println("adding the new  demo 4");
+	}
+}
